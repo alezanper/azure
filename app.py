236608fc027d7068efaef5d969e88223a69a6e37
fabenavideso@gmail.com <fabenavideso@gmail.com>
2021-08-29 21:10:37 -0500

    [added] dependencies

--- app.py
+++ app.py
@@ -1,30 +1,14 @@
-# from flask import Flask
-# from flask_restful import Resource, Api
-# 
-# app = Flask(__name__)
-# api = Api(app)
-# 
-# class Student(Resource):
-#     def get(self, name):
-#         return {'student': name}
-# 
-# api.add_resource(Student, '/student/<string:name>')
-# 
-# app.run()
-
-import os
-from flask import Flask
-import socket
-
-app = Flask(__name__)
-
-@app.route("/")
-def main():
-    return "<h1>My Demo</h1>" + "<p>A Simple Application for testing purposes.</p>" + "<h3>Hello from: " + socket.gethostname() + "</h3>" + "<button>Button</button>"
-
-@app.route('/<name>')
-def greeting(name):
-    return "<h3>Hello, " + name + "</h3>"
-
+ from flask import Flask
+ from flask_restful import Resource, Api
+ 
+ app = Flask(__name__)
+ api = Api(app)
+ 
+ class Student(Resource):
+     def get(self, name):
+         return {'student': name}
+ 
+ api.add_resource(Student, '/student/<string:name>')
+ 
 if __name__ == "__main__":
     app.run()
