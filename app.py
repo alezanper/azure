e4cfb7a2ea1fd9c02d262e3fbb599f7c31db8b73
fabenavideso@gmail.com <fabenavideso@gmail.com>
2021-08-29 21:16:30 -0500

    [added] dependencies

--- app.py
+++ app.py
@@ -1,14 +1,54 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
+items = []
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required= True,
+        help="This field cannot be left blank"
+    )
 
-api.add_resource(Student, '/student/<string:name>')
+    def get(self, name):
+        item = next(filter(lambda x: x['name']==name, items), None)
+        return {'item': item}, 200 if item is not None else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name']==name, items), None):
+            return {'message': "An item with name '{}' already exists".format(name)}, 400
+
+        data = Item.parser.parse_args()
+
+        #data = request.get_json()   # force=True silence=True
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name']!=name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        # data = request.get_json()   # force=True silence=True
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')    
 
 if __name__ == "__main__":
     app.run()
